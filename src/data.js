5eed4a8a68f629c4fe6e22da75147b9a0cb86e4e
Raulanthropos <ipsichias@yandex.com>
2024-07-14 18:13:12 +0300

    Removed a project

--- src/data.js
+++ src/data.js
@@ -8,15 +8,6 @@ export const projects = [
     github: "https://github.com/Raulanthropos/Marketplace",
     link: "https://marketplace-omega-tawny.vercel.app/",
   },
-  {
-    title: "Comfy Blog",
-    subtitle: "React, Redux, Node, Express, MongoDB",
-    description:
-      "This full-stack app, is a platform designed to raise awareness about social issues. Users can sign up to read all articles, post their own content, and edit their settings if needed. The app provides a user-friendly interface that allows for easy navigation and interaction.",
-    image: "https://res.cloudinary.com/dlhs6o8ga/image/upload/v1681716503/rjs_ijsr3d.png",
-    github: "https://github.com/Raulanthropos/blog-hosted",
-    link: "https://strive-blog-hosted.vercel.app",
-  },
 ];
 
 export const skills = [
