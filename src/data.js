0b44f8c932d55c3720e2aacb42166a774075046a
Raulanthropos <ipsichias@yandex.com>
2024-07-16 18:27:50 +0300

    Corrected the project's description

--- src/data.js
+++ src/data.js
@@ -3,8 +3,9 @@ export const projects = [
     title: "Marketplace",
     subtitle: "React, MUI, Redux, Zustand, Node, Express, MongoDB",
     description:
-    "A mock e-shop application built using React, Redux, Zustand, and Material-UI (MUI). The application is fully responsive and provides functionalities such as user registration, login, product viewing, adding reviews, and managing a shopping cart.",
-    image: "https://res.cloudinary.com/dlhs6o8ga/image/upload/v1720723296/eshop_fp8jhc.png",
+      "An e-shop application built using MERN, Redux, Zustand, and Material-UI (MUI). The application is fully responsive and provides functionalities such as user registration, login, product viewing, adding reviews, and managing a shopping cart.",
+    image:
+      "https://res.cloudinary.com/dlhs6o8ga/image/upload/v1720723296/eshop_fp8jhc.png",
     github: "https://github.com/Raulanthropos/Marketplace",
     link: "https://marketplace-omega-tawny.vercel.app/",
   },
